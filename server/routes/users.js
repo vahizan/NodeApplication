ac0dcffa989e92e24bc97d53551356eb2944ad8f
Manishthakur1297 <Manishthakur1297@gmail.com>
2019-07-30 00:13:38 +0530

    Create User Model and Prepare Routes

---
+++ server/routes/users.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+router.post('/auth', function(req, res){
+
+});
+
+router.post('/registration', function(req, res){
+
+});
+
+module.exports = router;
